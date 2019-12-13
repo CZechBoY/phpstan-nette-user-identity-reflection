8b919d9c503e55a43ca6b9c2215f9689b782a823
Adam Žurek <thejabko@gmail.com>
2019-12-13 19:10:44 +0100

    phpstan update (#1)
    added travis check

--- src/AdditionalReflections/UserIdentityReflectionExtension.php
+++ src/AdditionalReflections/UserIdentityReflectionExtension.php
@@ -6,10 +6,7 @@ use Nette\Security\IIdentity;
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\PropertiesClassReflectionExtension;
 use PHPStan\Reflection\PropertyReflection;
-use PHPStan\Type\ArrayType;
-use PHPStan\Type\IntegerType;
 use PHPStan\Type\StringType;
-use PHPStan\Type\TrueOrFalseBooleanType;
 use PHPStan\Type\Type;
 
 /**
@@ -25,42 +22,25 @@ class UserIdentityReflectionExtension implements PropertiesClassReflectionExtens
     public function __construct()
     {
         $this->properties = [
-            'browser' => new StringType(true),
-            'email'   => new StringType(true),
-            'ip'      => new StringType(true),
-            'login'   => new StringType(false),
-            'name'    => new StringType(true),
-            'passwd'  => new StringType(false),
+            'browser' => new StringType(),
+            'email'   => new StringType(),
+            'ip'      => new StringType(),
+            'login'   => new StringType(),
+            'name'    => new StringType(),
+            'passwd'  => new StringType(),
         ];
     }
 
-    /**
-     * @param ClassReflection $classReflection
-     * @param string $propertyName
-     *
-     * @return bool
-     */
     public function hasProperty(ClassReflection $classReflection, string $propertyName): bool
     {
         return $classReflection->getName() === IIdentity::class && array_key_exists($propertyName, $this->properties);
     }
 
-    /**
-     * @param ClassReflection $classReflection
-     * @param string $propertyName
-     *
-     * @return PropertyReflection
-     */
     public function getProperty(ClassReflection $classReflection, string $propertyName): PropertyReflection
     {
         return new UserIdentityPropertyReflection($classReflection, $this->getPropertyType($propertyName));
     }
 
-    /**
-     * @param string $propertyName
-     *
-     * @return Type
-     */
     private function getPropertyType(string $propertyName): Type
     {
         return $this->properties[$propertyName];
